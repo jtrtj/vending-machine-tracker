fccd884ebdd7026cbf2408949359dda1e0a28f43
jtrtj <jt.r@live.com>
2018-07-20 10:19:41 -0600

    test is green

--- app/models/machine.rb
+++ app/models/machine.rb
@@ -2,5 +2,5 @@ class Machine < ApplicationRecord
   belongs_to :owner
 
   has_many :machine_snacks
-  has_many :machines, through: :machine_snacks
+  has_many :snacks, through: :machine_snacks
 end
