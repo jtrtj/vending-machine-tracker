65a268d854869ca75914d6358c707e8d13a4d569
jtrtj <jt.r@live.com>
2018-07-22 21:24:17 -0600

    add final test and add snack_type_count to machine model

--- spec/features/snack_show_page_spec.rb
+++ spec/features/snack_show_page_spec.rb
@@ -6,6 +6,13 @@ I see the price for that snack
 I see a list of locations with vending machines that carry that snack
 I see the average price for snacks in those vending machines
 And I see a count of the different kinds of items in that vending machine.
+
+ex.
+Flaming Hot Cheetos
+Price: $2.50
+Locations
+* Don's Mixed Drinks (3 kinds of snacks, average price of $2.50)
+* Turing Basement (2 kinds of snacks, average price of $3.00)
 =end
 
 require 'rails_helper'
@@ -15,12 +22,22 @@ describe 'a user' do
     it 'will see see the snack attributes' do
       owner = Owner.create(name: "Sam's Snacks")
       dons  = owner.machines.create(location: "Don's Mixed Drinks")
-      snack_1 = dons.snacks.create(title: 'gfdas', price: 12.50)
-
+      turing  = owner.machines.create(location: "Turing Basement")
+      snack_1 = Snack.create(title: 'Flamin Hot Cheetos', price: 2.00)
+      snack_2 = Snack.create(title: 'Starbursts', price: 4.00)
+      MachineSnack.create(snack: snack_1, machine: dons)
+      MachineSnack.create(snack: snack_1, machine: turing)
+      MachineSnack.create(snack: snack_2, machine: turing)
+      
       visit snack_path(snack_1)
 
       expect(page).to have_content(snack_1.title)
       expect(page).to have_content("Price: $#{snack_1.price}")
+      expect(page).to have_content("* #{dons.location}")
+      expect(page).to have_content("* #{turing.location}")
+      expect(page).to have_content("* #{turing.average_snack_price}")
+      expect(page).to have_content("* #{dons.average_snack_price}")
+      
     end
   end
 end
